01ad6694dfb51aa57892ff458c95f963f6bca42f
navyasi <navyasi@my.ofs.edu.sg>
2021-09-26 21:48:19 +0800

    Create Grade1101.java
    created this new file from teamplate

---
+++ Grade1101.java
@@ -0,0 +1,22 @@
+/*
+
+PrX.Y:
+--Copy and paste the corresponding problem description
+  from the worksheets here--
+  
+*/
+
+public class Grade1101		// Replace "Template"with your specific class name
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Test match");
+	}
+}
+
+
+/*
+
+OUTPUT OF YOUR PROGRAM (copy & paste from Terminal/Command prompt)
+
+*/
