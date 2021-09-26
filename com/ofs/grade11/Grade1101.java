15b32f1f9e70ee9212f82385e6670115e1bda22c
navyasi <navyasi@my.ofs.edu.sg>
2021-09-26 22:05:07 +0800

    Create Grade1101.java
    add

---
+++ com/ofs/grade11/Grade1101.java
@@ -0,0 +1,24 @@
+package com.ofs.grade11;/*
+
+PrX.Y:
+--Copy and paste the corresponding problem description
+  from the worksheets here--
+  
+*/
+
+public class Grade1101		// Replace "Template"with your specific class name
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Test match");
+		System.out.println("Test match - change ");
+		System.out.println("Test match - Fetch ");
+	}
+}
+
+
+/*
+
+OUTPUT OF YOUR PROGRAM (copy & paste from Terminal/Command prompt)
+
+*/
