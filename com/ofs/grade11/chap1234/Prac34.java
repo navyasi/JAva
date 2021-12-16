271cc86ac707b859767c6f5dbaeb5cde581defbe
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2021-12-16 09:53:01 +0800

    Update Prac34.java

--- com/ofs/grade11/chap1234/Prac34.java
+++ com/ofs/grade11/chap1234/Prac34.java
@@ -1,18 +1,18 @@
 package com.ofs.grade11.chap1234;
 
 public class Prac34 {
-    public static void main(String [] args){
+    public static void main(String [] args) {
         int n = IBIO.inputInt("Enter a number: ");
         int counter = 0;
-        do{
-            if (n % 2 == 0){
+        do {
+            if (n % 2 == 0) {
                 n = n/2;
             }
             else
                 n = 3*n + 1;
             counter = counter + 1;
             IBIO.out(n + ", ");
-        }while(n>1);
+        } while(n>1);
         IBIO.output(counter + " steps taken.");
     }
 }
